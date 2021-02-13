52d3cab8099d11ade26daddb922e15ece0c90cb4
Ahmet <ahmetalperwork@gmail.com>
2021-02-13 16:12:12 +0300

    remove netflix populars due to API not catching any trailers for the categories

--- src/App.js
+++ src/App.js
@@ -12,14 +12,14 @@ function App() {
       <Nav />
       <Banner fetchUrl={requests.fetchNetflixOriginals}/>
       <div className="rows">
-        <Row id="1" title="Popular on Netflix" fetchUrl={requests.fetchNetflixOriginals}/>
-        <Row id="2" title="Trending Now" fetchUrl={requests.fetchTrending}/>
-        <Row id="3" title="Top Rated" fetchUrl={requests.fetchTopRated}/>
-        <Row id="4" title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
-        <Row id="5" title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
-        <Row id="6" title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
-        <Row id="7" title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
-        <Row id="8" title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+        {/* <Row id="1" title="Popular on Netflix" fetchUrl={requests.fetchNetflixOriginals}/> */}
+        <Row id="1" title="Trending Now" fetchUrl={requests.fetchTrending}/>
+        <Row id="2" title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+        <Row id="3" title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+        <Row id="4" title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+        <Row id="5" title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+        <Row id="6" title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+        <Row id="7" title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
       </div>
     </div>
   );
